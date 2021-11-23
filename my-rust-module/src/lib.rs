5bc729877810d6b208a5383735d60c03e57fddbd
Shohei Jinno <shohei.jinno@gmail.com>
2021-11-23 15:49:08 -0800

    Add an example.

--- my-rust-module/src/lib.rs
+++ my-rust-module/src/lib.rs
@@ -1,19 +1,42 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+use observable_react::ReactObservable;
+use observable_rs::Observable;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    log::set_logger(&wasm_bindgen_console_logger::DEFAULT_LOGGER).unwrap();
+    log::set_max_level(log::LevelFilter::Info);
+    crate::utils::set_panic_hook();
+}
+
+#[allow(unused)]
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub fn create_rust_thing() -> RustThing {
+    RustThing::default()
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, my-rust-module!");
+#[derive(Clone, Default)]
+pub struct RustThing {
+    things: usize,
+    list: Observable<Vec<String>>,
+}
+
+#[wasm_bindgen]
+impl RustThing {
+    pub fn do_something(&mut self) {
+        self.things += 1;
+        self.list.push(format!("Thing {}", self.things));
+    }
+    pub fn get_the_list(&self) -> ReactObservable {
+        self.list.clone().into()
+    }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useMemo, useReducer } from "react";
+import { useObserve } from "observable-rs";
 
-function App() {
-  return <div className="App">Hello, world!</div>;
+function App({ wasm }: { wasm: any }) {
+  const [listVisible, toggleShow] = useReducer((show: boolean) => !show, true);
+
+  const [thing, theList] = useMemo(() => {
+    const thing = wasm.create_rust_thing();
+    setInterval(() => thing.do_something(), 1000);
+    return [thing, thing.get_the_list()];
+  }, [wasm]);
+
+  return (
+    <div className="App">
+      <button onClick={toggleShow}>
+        {listVisible ? "Hide the list" : "Show the list"}
+      </button>
+      <br />
+      {listVisible ? <TheList theList={theList} /> : ""}
+    </div>
+  );
 }
 
 export default App;
+
+const TheList = ({ theList }: { theList: any }) => {
+  useObserve(theList);
+
+  return (
+    <div>
+      <h2>The List:</h2>
+      <ul>
+        {theList.map((v: any) => (
+          <li key={v}>{v}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
