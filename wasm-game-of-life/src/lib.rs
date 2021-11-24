2aae48173259149834e1a53a8a5dcaec2a38074c
Shohei Jinno <shohei.jinno@gmail.com>
2021-11-24 10:13:50 -0800

    Create Universe struct.

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -26,6 +26,39 @@ pub enum Cell {
     Alive = 1,
 }
 
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn count_live_neightbors(&self, row: u32, column: u32) -> u8 {
+        let mut counter = 0;
+
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+
+                counter += self.cells[idx] as u8;
+            }
+        }
+
+        counter
+    }
+}
+
 // #[allow(unused)]
 // #[wasm_bindgen]
 // pub fn create_game_of_life() -> GameOfLife {
