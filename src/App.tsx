517dec435ef7ff6443af34a33da3934c2cde27aa
Shohei Jinno <shohei.jinno@gmail.com>
2021-11-24 09:09:09 -0800

    Test alert function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App({ wasm }: { wasm: any }) {
   console.log(wasm);
+  useEffect(() => {
+    const alertOnce = () => wasm.greet();
+    alertOnce();
+  }, [wasm]);
 
   return <div className="App">Hello, world!</div>;
 }
